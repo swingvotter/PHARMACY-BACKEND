044b156e35bb3ef790b638065efbbc32d771c178
unknown <swingvotter@gmail.com>
2025-04-24 22:33:51 -1200

    added hosted frontend

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(
       "http://localhost:3000",
       "http://localhost:3001",
       "http://127.0.0.1:3001",
-      "http://127.0.0.1:5500",
+      "https://medipharm-phi.vercel.app",
     ],
     credentials: true,
   })
